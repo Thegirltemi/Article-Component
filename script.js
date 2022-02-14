0ffffdc955f98e36e6b7fa3f8613ec9c0aeaef69
thegirltemi <temialamutu7@gmail.com>
2022-02-14 05:28:25 -0800

    init

---
+++ script.js
@@ -0,0 +1,9 @@
+const socials=document.querySelector('.openshare');
+const shareBtn=document.getElementById('share');
+
+
+shareBtn.addEventListener('click',()=>{
+    socials.classList.add('clicked')
+    
+});
+
